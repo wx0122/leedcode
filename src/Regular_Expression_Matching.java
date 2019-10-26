7b9ca2db0a1be9582985fd2335adbe6f15b40d01
xuanwang <xuanwang@freewheel.tv>
2019-10-26 17:25:35 -0400

    Adding first file

---
+++ src/Regular_Expression_Matching.java
@@ -0,0 +1,15 @@
+public class Regular_Expression_Matching {
+    public static boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
